1ff739d64e1bb31aeca92744cd297915e639ce65
Filipe Melo <f.melo@marquesconsult.com.br>
2022-04-18 00:17:35 -0300

    decrease coverage

--- person.go
+++ person.go
@@ -8,6 +8,10 @@ func (p Person) GetName() string {
 	return p.name
 }
 
+func (p Person) GetDoubledName() string {
+	return p.name + p.name
+}
+
 func NewPerson(personName string) Person {
 	return Person{
 		name: personName,
